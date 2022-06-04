dfa17820975c2a41ad11decfe0642bdc1f6b2230
Dang Huu Hoai Nam <dhhn2002@gmail.com>
2022-06-04 11:06:38 +0700

    add sanpham and loaisanpham model

--- ASM_Shepoo/src/main/java/com/poly/model/LoaiSanPham.java
+++ ASM_Shepoo/src/main/java/com/poly/model/LoaiSanPham.java
@@ -1,5 +1,31 @@
 package com.poly.model;
 
-public class LoaiSanPham {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "LoaiSanPham")
+public class LoaiSanPham {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "MaLSP")
+	private int maLSP;
+	@Column(name = "TenLSP")
+	private String tenLSP;
+	@OneToMany(mappedBy = "loaiSanPham")
+	private List<SanPham> sanPhams;
 }

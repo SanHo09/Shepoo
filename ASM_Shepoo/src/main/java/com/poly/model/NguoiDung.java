351da6e3a02b39e0f313dc7ca4574670901227f5
Tarashaki4s <tarashaki4s@gmail.com>
2022-06-04 11:10:51 +0700

    map model thong bao va Nguoi dung

--- ASM_Shepoo/src/main/java/com/poly/model/NguoiDung.java
+++ ASM_Shepoo/src/main/java/com/poly/model/NguoiDung.java
@@ -1,5 +1,32 @@
 package com.poly.model;
 
-public class NguoiDung {
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="nguoidung")
+public class NguoiDung {
+	@Id
+	int maND;
+	String username;
+	String sdt;
+	String password;
+	boolean gioitinh;
+	Date ngaysinh;
+	String diachi;
+	boolean vaitro;
+	String tenshop;
+	String tennguoidung;
+	String email;
+	String image;
+	@OneToMany(mappedBy = "nguoidung")
+	List<ThongBao> thongbao;
 }
--- ASM_Shepoo/src/main/java/com/poly/model/ThongBao.java
+++ ASM_Shepoo/src/main/java/com/poly/model/ThongBao.java
@@ -1,5 +1,22 @@
 package com.poly.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="thongbao")
 public class ThongBao {
+	@Id
+	int maTB;
+	String noidung;
+	int maND;
+	@ManyToOne@JoinColumn(name = "maND")
+	NguoiDung nguoidung;
 
 }
